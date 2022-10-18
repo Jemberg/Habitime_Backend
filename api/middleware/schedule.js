bb75a8ea14e931f1cf1b41ffb20bf4c4a27e47fd
MartinJemberg <martins.jembergs@gmail.com>
2022-10-19 00:10:13 +0200

    Firebase implementation fix V3.

--- api/middleware/schedule.js
+++ api/middleware/schedule.js
@@ -6,9 +6,9 @@ const admin = require("firebase-admin");
 // Firebase initialization.
 admin.initializeApp({
   credential: admin.credential.cert({
-    "project_id": process.env.PROJECT_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
-    "client_email": process.env.CLIENT_EMAIL
+    project_id: process.env.PROJECT_ID,
+    private_key: process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+    client_email: process.env.CLIENT_EMAIL
   }),
   databaseURL: `${process.env.FIREBASE_DB_URL}`,
 });
